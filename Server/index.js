6bc6496bbdc5d1d04889a486738728517f35c03b
Hardik Gupta <84801793+ghardik25@users.noreply.github.com>
2025-02-07 12:04:57 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const router = require('./routes/routes');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 var corsOptions = {
@@ -17,5 +17,5 @@ app.use(express.json());
 app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Example app: Listening on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
